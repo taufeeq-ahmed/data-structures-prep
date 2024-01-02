e32f02f98c149dae05930201b6d7003db9963f73
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-01-02 16:20:53 +0530

    linkedlist class also defined

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,6 +1,12 @@
 package LinkedList;
 
 public class LinkedList {
-    Node head;
-    Node tail;
+    Node head, tail;
+    int length;
+
+    LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
 }
