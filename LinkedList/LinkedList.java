72d2c8fe690fe6ae25ffd7b8ee66f15403f41cb7
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-01-02 16:53:58 +0530

    push and pop methods are added

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -9,4 +9,56 @@ public class LinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    public void push(int val) {
+        Node newNode = new Node(val);
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+
+    public int pop() {
+        Node current = this.head;
+        Node prev = this.head;
+
+        if (this.length > 1) {
+            while (current.next != null) {
+                prev = current;
+                current = current.next;
+            }
+
+            prev.next = null;
+            this.tail = prev;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+        this.length--;
+
+        return current.val;
+
+    }
+
+    public void print() {
+        if (this.length == 0) {
+            System.out.print("[]");
+            return;
+        }
+
+        Node current = this.head;
+
+        System.out.print("[");
+        while (current != null) {
+            System.out.print(current.val + " , ");
+            current = current.next;
+        }
+        System.out.print("]");
+
+    }
 }
--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -4,8 +4,8 @@ public class Node {
     int val;
     Node next;
 
-    Node(int val, Node next) {
+    Node(int val) {
         this.val = val;
-        this.next = next;
+        this.next = null;
     }
 }
