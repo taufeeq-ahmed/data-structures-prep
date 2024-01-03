9534be698eb337af277879b2d029a0dc5bdc66af
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-01-03 13:24:13 +0530

    insert method added in the tree

--- BST/BST.java
+++ BST/BST.java
@@ -6,4 +6,33 @@ public class BST {
     BST() {
         this.root = null;
     }
+
+    public void insert(int val) {
+        if (this.root == null) {
+            this.root = new Node(val);
+            return;
+        }
+
+        Node current = root;
+        while (true) {
+            if (current.val == val)
+                return;
+
+            if (current.val > val) {
+                if (current.left == null) {
+                    current.left = current;
+                    return;
+                } else
+                    current = current.left;
+            } else {
+                if (current.right == null) {
+                    current.right = current;
+                    return;
+                } else
+                    current = current.right;
+            }
+        }
+
+    }
+
 }
